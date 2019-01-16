19cff56a3912320de323536e3de4633f2c9b0b1d
Alexabeatrice22 <alexabeatrice22@gmail.com>
2019-01-16 19:31:08 +0200

    Functions

--- src/com/SIBV/CalculateMax.java
+++ src/com/SIBV/CalculateMax.java
@@ -25,28 +25,3 @@ public class CalculateMax {
         return max;
     }
 }
-    class CalcMax2 {
-public static void main(String[] args) {
-        System.out.println("Enter the first number: ");
-        Scanner keyboard = new Scanner(System.in);
-        int a = keyboard.nextInt();
-        System.out.println("Enter the second number: ");
-        int b = keyboard.nextInt();
-        System.out.println("Enter the third number: ");
-        int c = keyboard.nextInt();
-        int d = getMax(a, b, c);
-        System.out.println("Maximum Value = " + d);
-        }
-
-public static int getMax(int n1, int n2, int n3) {
-        int max;
-        if (n1 > n2 && n1 > n3)
-        max = n1;
-        else if (n2 > n3)
-        max = n2;
-        else
-        max = n3;
-
-        return max;
-   }
-}
